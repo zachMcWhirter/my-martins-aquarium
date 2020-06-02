4c23813773953522380753f35c78f583841070ae
Zach McWhirter <zmac7811@gmail.com>
2020-06-02 13:36:13 -0500

    added pictures of fish

--- scripts/fish/fishData.js
+++ scripts/fish/fishData.js
@@ -3,7 +3,7 @@ const fishCollection = [
         species: "Trigger Fish",
         length: 12,
         location: "Bay of Bengal",
-        picture: "./images/triggerfish.jpg",
+        picture: "https://i.redd.it/yma5wuk73hm01.jpg",
         food: "Baby squid",
         name: "Bubbles"
     },
@@ -19,7 +19,7 @@ const fishCollection = [
         species: "Swordfish",
         length: 320,
         location: "Atlantic Ocean",
-        picture: "./images/chester.jpeg",
+        picture: "https://i.pinimg.com/236x/bc/85/46/bc85464a5f6bedf949d5e12ad8cd6882--marlin-fishing-deep-sea-fishing.jpg",
         food: "Herring",
         name: "Chester"
     },
@@ -27,7 +27,7 @@ const fishCollection = [
         species: "Angel Fish",
         length: 8,
         location: "Pacific Ocean",
-        picture: "./images/angelfish.jpg",
+        picture: "https://c402277.ssl.cf1.rackcdn.com/photos/1803/images/story_full_width/%28c%29_Jonathan_Rossouw_IMG_9336.jpg?1345603462",
         food: "Plankton",
         name: "Michael"
     },
@@ -37,13 +37,13 @@ const fishCollection = [
         location: "Caribbean Sea",
         food: "Algae",
         name: "Sweet",
-        picture: "./images/pink.jpg"
+        picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Corydoras_elegans.jpg/440px-Corydoras_elegans.jpg"
     },
     {
         species: "Orange Roughy",
         length: 16,
         location: "Southern Ocean",
-        picture: "./images/roughy.jpeg",
+        picture: "https://i.pinimg.com/236x/9d/3e/2b/9d3e2b9a8aa093f73079e995fcf2995c.jpg",
         food: "Anchovies",
         name: "Michael"
     }
